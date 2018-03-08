3bd821a975a0a5882bdd7517dfc96d7229776624
oconorr3 <oconorr3@vt.edu>
2018-03-07 20:16:04 -0500

    Redesigned HomePage using bootstrap template 'jumbotron'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,58 +12,102 @@ import { NavDropdown } from 'react-bootstrap'
 import { MenuItem } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap';
 
+import { LoginPage } from './LoginPage.jsx'
+
 class Home extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
+          username: '',
+          password: '',
+          submitted: false
       };
 
   }
+
+  handleEmailChange(e){
+     this.setState({email: e.target.value});
+  }
+
+  handlePasswordChange(e) {
+     this.setState({password: e.target.value});
+  }
+
+  handleSignIn(e) {
+      e.preventDefault();
+
+      this.setState({ submitted: true });
+      const { username, password } = this.state;
+      const { dispatch } = this.props;
+      if (username && password) {
+          //dispatch(userActions.login(username, password));
+          console.log("Email: " + this.state.email);
+          console.log("Password: " + this.state.password);
+
+          //Not doing any real authentication right now
+      }
+  }
+
   render() {
     return (
       <div>
-        <div>
-          <Navbar inverse collapseOnSelect>
-              <Navbar.Header>
-                <Navbar.Brand>
-                  <a>Maptivity</a>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-              </Navbar.Header>
-              <Navbar.Collapse>
-                <Nav>
-                  <NavItem eventKey={1} href="#">
-                    About
-                  </NavItem>
-                  <NavItem eventKey={2} href="#">
-                    Our Mission
-                  </NavItem>
-                  <NavDropdown eventKey={3} title="Products & Features" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Data Analytics</MenuItem>
-                    <MenuItem eventKey={3.2}>Data Vizualization</MenuItem>
-                    <MenuItem eventKey={3.3}>Data Predictions</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.3}>link to somewhere</MenuItem>
-                  </NavDropdown>
-                </Nav>
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Maptivity</a>
+            </div>
+            <div id="navbar" className="navbar-collapse collapse">
+              <form className="navbar-form navbar-right" action="this.handle">
+                <div className="form-group">
+                  <input type="text" placeholder="Email" className="form-control" onChange={this.handleEmailChange}></input>{' '}
+                </div>
+                <div className="form-group">
+                  <input type="password" placeholder="Password" className="form-control"onChange={this.handlePasswordChange}></input>{' '}
+                </div>
+                <button type="submit" className="btn btn-success">Sign In</button>
+              </form>
+            </div>
+          </div>
+        </nav>
 
-                <Nav pullRight>
-                  <LinkContainer to="/login">
-                    <NavItem eventKey={1} href="#">
-                      Login
-                    </NavItem>
-                  </LinkContainer>
-                  <LinkContainer to="/register">
-                    <NavItem eventKey={2} href="#">
-                      Register
-                    </NavItem>
-                  </LinkContainer>
-                </Nav>
-              </Navbar.Collapse>
-          </Navbar>
+        <div className="jumbotron">
+          <div className="container">
+            <h1>Maptivity</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          </div>
+        </div>
 
-      </div>
-        <h2>Our Mission: Providing Your Organization With The Information You Need To Be Successfull</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <h2>Services</h2>
+              <p>Some text that explains the services we have and how we raise rabble.</p>
+              <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div className="col-md-4">
+              <h2>Products</h2>
+              <p>Some text that explains what kind of products we offer.</p>
+              <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+           </div>
+            <div className="col-md-4">
+              <h2>About Us</h2>
+              <p>Some text that explains who we are are and how much we like to raise some rabble.</p>
+              <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div>
+          </div>
+
+
+          <footer>
+            <p>&copy; 2016 The Rabble Rousers, Inc.</p>
+          </footer>
+        </div>
       </div>
     );
   }
