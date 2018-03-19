274dfb087cd5c90b10dd25fc93876599a2fb6314
DakDoug <c0d3ster@vt.edu>
2018-03-19 01:43:38 -0400

    brought in fading bubbles plugin code manually and made it more concise, added stage 2 for spread syntax and class properties transform

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [ 'react', 'env' ]
+            presets: [ 'react', 'env', 'stage-2' ]
           }
         },
         exclude: /node_modules/
