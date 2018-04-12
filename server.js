1abc098c99b109f3b94477886b3f2b89956aea29
kasrae9 <kasrae9@vt.edu>
2018-04-12 00:25:49 -0400

    Wire socket.io with express. Partially parse 2017 csv file and beautify JSON to transmit timestamp and lat and long over the socket to the client.

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const path = require('path');
 const express = require('express');
-const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
+const csv = require('csv');
+const phoneDataSeventeen = require('./2017phoneData.js');
 
 const DIST_DIR = path.join(__dirname, 'public/');
 const PORT = 3000;
 
+const app = express();
 app.use(express.static(DIST_DIR));
+
+const server = http.createServer(app);
+
+//wire up ExpressJS server to Socket.IO
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("Connected to client and here is phone data: " + JSON.stringify(phoneDataSeventeen.phoneData)), setInterval(
+    () => socket.emit('testEvent', "Message from server sent"),
+    5000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 
-app.listen(PORT, () => console.log("Maptivity server listening on port " + PORT));
+server.listen(PORT, () => console.log("Maptivity server listening on port " + PORT));
